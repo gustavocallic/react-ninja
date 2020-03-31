b6362df9b8a5514bcde2b659353113f801ed02f6
gustavo <gustavo@ansata.com.br>
2020-03-31 16:16:25 -0300

    commit

--- src/Title.js
+++ src/Title.js
@@ -58,11 +58,15 @@ class Form extends React.Component {
         this.input = React.createRef();
         this.txtNome = React.createRef();
         this.checkbox = React.createRef();
+        this.radio = React.createRef();
         this.Submit = this.Submit.bind(this)
         this.mudaValorTxtComponenteControlado = this.mudaValorTxtComponenteControlado.bind(this)
         this.state = {
             sobrenome: 'Miranda',
-            checked: false
+            checked: false,
+            radioControlValue: '1',
+            selectValue: '1',
+            textAreaValue: ''
         }
 
     }
@@ -70,6 +74,9 @@ class Form extends React.Component {
     mudaValorTxtComponenteControlado(e) {
         console.log(e.target.value)
         this.setState({ sobrenome: e.target.value })
+    }
+    mudarRadioBtnControl(e) {
+        this.setState({ radioControlValue: e.target.value })
     }
 
     Submit(e) {
@@ -93,11 +100,44 @@ class Form extends React.Component {
                     </div>
                     <div>
                         <label>
-                            <input type='checkbox' value='123' defaultChecked={false} ref={this.checkbox} 
-                            onChange={() => console.log(this.checkbox.current.checked)} />
+                            <input type='checkbox' value='123' defaultChecked={false} ref={this.checkbox}
+                                onChange={() => console.log(this.checkbox.current.checked)} />
                             <span>Checkbox sem controle</span>
                         </label>
                     </div>
+                    <div>
+                        <label>
+                            <input type='radio' value='1' name="radiobtn" checked={this.state.radioControlValue === '1'} onChange={(e) => this.mudarRadioBtnControl(e)} />
+                            <input type='radio' value='2' name="radiobtn" checked={this.state.radioControlValue === '2'} onChange={(e) => this.mudarRadioBtnControl(e)} />
+                            <span>Radio Controlado</span>
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            <input type='radio' name="radiobtna" value="1" />
+                            <input type='radio' name="radiobtna" value="2" />
+                            <span>Radio sem Controlado (nao usar este)</span>
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            <label>Select Controled</label>
+                            <select value={this.state.selectValue} onChange={(e) => {
+                                this.setState({ selectValue: e.target.value })
+                            }}>
+                                <option value='1'>Opt 1</option>
+                                <option value='2'>Opt 2</option>
+                                <option value='3'>Opt 3</option>
+                            </select>
+                        </label>
+                    </div>
+
+                    <div>
+                        <label>
+                            <textarea value={this.state.textAreaValue} onChange={(e) => {this.setState({textAreaValue: e.target.value})}}/>
+                        </label>
+                    </div>
+
                     <button type='submit'>Submit</button>
                 </form>
             </Fragment>
